5949db757e306d6c6cf73b5a6e8f0eda80f4c2cc
drolon <drolon@f6e0daa0-2725-47c6-9c0b-5e6e9cdd0720>
2011-10-30 11:43:46 +0000

    FreeImage now uses LibRaw 0.14.3

--- Source/LibRawLite/dcraw/dcraw.c
+++ Source/LibRawLite/dcraw/dcraw.c
@@ -19,8 +19,8 @@
    *If you have not modified dcraw.c in any way, a link to my
    homepage qualifies as "full source code".
 
-   $Revision: 1.26 $
-   $Date: 2011-10-23 20:45:37 $
+   $Revision: 1.27 $
+   $Date: 2011-10-30 11:43:46 $
  */
 
 #define DCRAW_VERSION "9.11"
